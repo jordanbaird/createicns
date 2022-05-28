2fa5652578d40d504c806a9229582982d7fd951c
Jordan Baird <baird99.j@gmail.com>
2022-05-28 09:31:15 -0600

    Refactor `IconSet`

--- Sources/IconSet.swift
+++ Sources/IconSet.swift
@@ -10,55 +10,67 @@ import Cocoa
 
 /// Represents an 'iconset' file.
 struct IconSet {
-  typealias Icon = (data: Data, name: String)
-  
-  private let dimensions: [(CGFloat, CGFloat, String)] = [
-    (16,   16,   "16x16"),
-    (32,   32,   "16x16@2x"),
-    (32,   32,   "32x32"),
-    (64,   64,   "32x32@2x"),
-    (128,  128,  "128x128"),
-    (256,  256,  "128x128@2x"),
-    (256,  256,  "256x256"),
-    (512,  512,  "256x256@2x"),
-    (512,  512,  "512x512"),
-    (1024, 1024, "512x512@2x")
-  ]
-  
-  private(set) var icons = [Icon]()
-  
-  init(image: NSImage) throws {
-    icons = try dimensions.map {
-      try createIcon(from: image, width: $0.0, height: $0.1, name: $0.2)
+  struct Dimension {
+    let width: CGFloat
+    let height: CGFloat
+    let suffix: String
+    
+    func createIcon(from image: NSImage) throws -> Icon {
+      let size = CGSize(width: width / 2, height: height / 2)
+      let newImage = NSImage(size: size)
+      
+      newImage.lockFocus()
+      image.draw(
+        in: .init(origin: .zero, size: size),
+        from: .init(origin: .zero, size: image.size),
+        operation: .sourceOver,
+        fraction: 1)
+      newImage.unlockFocus()
+      
+      guard
+        let tiffRep = newImage.tiffRepresentation,
+        let bitmapRep = NSBitmapImageRep(data: tiffRep),
+        let pngData = bitmapRep.representation(using: .png, properties: [:])
+      else {
+        throw CreationError("Could not create data for iconset.")
+      }
+      
+      return Icon(data: pngData, suffix: suffix)
     }
   }
   
-  private func createIcon(
-    from image: NSImage,
-    width: CGFloat,
-    height: CGFloat,
-    name: String
-  ) throws -> Icon {
-    let size = CGSize(width: width / 2, height: height / 2)
-    let newImage = NSImage(size: size)
+  struct Icon {
+    let data: Data
+    let suffix: String
     
-    newImage.lockFocus()
-    image.draw(
-      in: .init(origin: .zero, size: size),
-      from: .init(origin: .zero, size: image.size),
-      operation: .sourceOver,
-      fraction: 1)
-    newImage.unlockFocus()
-    
-    guard
-      let tiffRep = newImage.tiffRepresentation,
-      let bitmapRep = NSBitmapImageRep(data: tiffRep),
-      let pngData = bitmapRep.representation(using: .png, properties: [:])
-    else {
-      throw CreationError("Could not create data for iconset.")
+    func write(to url: URL) throws {
+      do {
+        try data.write(to: url.appendingPathComponent("icon_" + suffix + ".png"))
+      } catch {
+        throw CreationError(error.localizedDescription)
+      }
     }
-    
-    return Icon(data: pngData, name: name)
+  }
+  
+  private static let dimensions: [Dimension] = [
+    .init(width: 16, height: 16, suffix: "16x16"),
+    .init(width: 32, height: 32, suffix: "16x16@2x"),
+    .init(width: 32, height: 32, suffix: "32x32"),
+    .init(width: 64, height: 64, suffix: "32x32@2x"),
+    .init(width: 128, height: 128, suffix: "128x128"),
+    .init(width: 256, height: 256, suffix: "128x128@2x"),
+    .init(width: 256, height: 256, suffix: "256x256"),
+    .init(width: 512, height: 512, suffix: "256x256@2x"),
+    .init(width: 512, height: 512, suffix: "512x512"),
+    .init(width: 1024, height: 1024, suffix: "512x512@2x")
+  ]
+  
+  let icons: [Icon]
+  
+  init(image: NSImage) throws {
+    icons = try Self.dimensions.map {
+      try $0.createIcon(from: image)
+    }
   }
   
   func write(to url: URL) throws {
@@ -68,11 +80,7 @@ struct IconSet {
       throw CreationError(error.localizedDescription)
     }
     for icon in icons {
-      do {
-        try icon.data.write(to: url.appendingPathComponent("icon_" + icon.name + ".png"))
-      } catch {
-        throw CreationError(error.localizedDescription)
-      }
+      try icon.write(to: url)
     }
   }
   
