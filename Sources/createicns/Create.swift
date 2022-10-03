12e38fd065f92da2ff3c275ed42906d08c9010e7
Jordan Baird <jordanbaird.dev@gmail.com>
2022-10-03 00:52:49 -0600

    Bump version 0.0.1 -> 0.0.2

--- Sources/createicns/Create.swift
+++ Sources/createicns/Create.swift
@@ -15,7 +15,7 @@ struct Create: ParsableCommand {
   static var configuration: CommandConfiguration = {
     var configuration = CommandConfiguration()
     configuration.commandName = "createicns"
-    configuration.version = "0.0.1"
+    configuration.version = "0.0.2"
     return configuration
   }()
   
