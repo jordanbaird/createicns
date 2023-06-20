35529de2c73f6dbb6bbbff3a08c690666ebe92dc
Jordan Baird <jordanbaird.dev@gmail.com>
2023-06-19 18:14:57 -0600

    Define available platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "createicns",
+    platforms: [
+        .macOS(.v10_13),
+    ],
     products: [
         .executable(name: "createicns", targets: ["Frontend"]),
     ],
